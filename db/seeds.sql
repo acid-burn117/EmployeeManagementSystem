60d9e6362db82e5b524299762bb4a1a8d5d404af
acid-burn117 <osoriodouglas19@gmail.com>
2022-03-04 19:33:23 -0500

    database setup

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO department (name)
+VALUES  ("Sales")
+        ("Enginerring")
+        ("Human Resources")
+        ("Finance")
+        ("Legal")
+        ("Research and Development")
+
+INSERT INTO role (title, salary, department)
